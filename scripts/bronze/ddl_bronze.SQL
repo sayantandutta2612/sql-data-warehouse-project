07b397dff0984d2ad48110f99a190cbf8fa5c018
Sayantan Dutta <sayantandutta.2612@gmail.com>
2025-12-07 00:53:37 +0530

    Add documentation to ddl_bronze.SQL
    Add header comment to DDL script for Bronze tables.

--- scripts/bronze/ddl_bronze.SQL
+++ scripts/bronze/ddl_bronze.SQL
@@ -1,1 +1,11 @@
+/*
+===============================================================================
+DDL Script: Create Bronze Tables
+===============================================================================
+Script Purpose:
+    This script creates tables in the 'bronze' schema, dropping existing tables 
+    if they already exist.
+	  Run this script to re-define the DDL structure of 'bronze' Tables
+===============================================================================
+*/
 
